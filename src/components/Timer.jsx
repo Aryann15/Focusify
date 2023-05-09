c7a203bd85d047bdfa057da1603894ee3f119077
Aryann15 <rajlysm15@gmail.com>
2023-05-09 17:18:58 +0530

    using the useEffect and useRef

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import Block from "./Block";
 import "./Timer.css";
 import { timers } from "jquery";
@@ -7,6 +7,18 @@ import { timers } from "jquery";
 const Timer = ({timeLeft, setTimeLeft, isPaused, setIsPause}) => {
   let minutes = Math.floor(timeLeft / 60);
   let seconds = Math.floor(timeLeft - 60 * minutes);
+
+  let intervalRef= useRef();
+  useEffect(()=>{
+    const id = setInterval(()=>{
+        setTimeLeft(timeLeft-1)
+    },1000)
+  intervalRef.current=id;
+  return () => clearInterval(intervalRef.current);
+  }
+  )
+
+  
 
   return (
     <div className="container">
