bef98819de22be1a0437128ff6c98eda0f0c4c9c
Aryann15 <rajlysm15@gmail.com>
2023-05-09 17:47:21 +0530

    button for resetting the timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,29 +1,45 @@
-import React, {useEffect, useRef} from "react";
+import React, { useEffect, useRef } from "react";
 import Block from "./Block";
 import "./Timer.css";
 import { timers } from "jquery";
 
-
-const Timer = ({timeLeft, setTimeLeft, isPaused, setIsPause}) => {
+const Timer = ({ timeLeft, setTimeLeft, isPaused, setIsPaused }) => {
   let minutes = Math.floor(timeLeft / 60);
   let seconds = Math.floor(timeLeft - 60 * minutes);
 
-  let intervalRef= useRef();
-  useEffect(()=>{
-    const id = setInterval(()=>{
-        setTimeLeft(timeLeft-1)
-    },1000)
-  intervalRef.current=id;
-  return () => clearInterval(intervalRef.current);
-  }
-  )
-
-  
+  let intervalRef = useRef();
+  useEffect(() => {
+    const id = setInterval(() => {
+      {
+        isPaused
+          ? clearInterval(intervalRef.current)
+          : setTimeLeft(timeLeft - 1);
+      }
+    }, 1000);
+    intervalRef.current = id;
+    return () => clearInterval(intervalRef.current);
+  });
 
   return (
-    <div className="container">
-      <Block param="Minutes" number={minutes}/><h1>: </h1><Block param="Seconds" number={seconds}/>
-    </div>
+    <>
+      <div className="container">
+        <Block param="Minutes" number={minutes} />
+        <div className="semi">
+          <h1>:</h1>
+        </div>
+        <Block param="Seconds" number={seconds} />
+      </div>
+      <div class="butt">
+        <button
+          onClick={() => {
+            setTimeLeft(0);
+            setIsPaused(true);
+          }}
+        >
+          RESET
+        </button>
+      </div>
+    </>
   );
 };
 
