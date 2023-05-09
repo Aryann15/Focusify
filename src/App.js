ab6b642ca3745d631bcfe46425ea31750098de09
Aryann15 <rajlysm15@gmail.com>
2023-05-09 11:07:54 +0530

    logic for ternary operator

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import Form from "./components/Form";
 import Timer from "./components/Timer";
 import Finished from "./components/Finished";
 
 function App() {
-
-  const[timeLeft , setTimeLeft] = useState (true);
-  
+  const [timeLeft, setTimeLeft] = useState(0);
+  const [isPaused, setIsPaused] = useState(true);
   return (
     <>
-    <Form />
-    {
-      timeLeft ? <Timer /> : <Finished />
-    }
+      <Form setTimeLeft={setTimeLeft} />
+      {timeLeft < 0 ? (
+        <Finished setTimeLeft={setTimeLeft} setIsPaused={setIsPaused} />
+      ) : (
+        <Timer
+          timeLeft={timeLeft}
+          setTimeLeft={setTimeLeft}
+          isPaused={isPaused}
+          setIsPaused={setIsPaused}
+        />
+      )}
     </>
   );
 }
