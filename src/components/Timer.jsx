d59793f69b2f762467b253956dcc2f5e3c0054dd
Aryann15 <rajlysm15@gmail.com>
2023-05-09 16:59:57 +0530

    calculating the time

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Block from "./Block";
+import "./Timer.css";
+import { timers } from "jquery";
 
-const Timer = () => {
+
+const Timer = ({timeLeft, setTimeLeft, isPaused, setIsPause}) => {
+  let minutes = Math.floor(timeLeft / 60);
+  let seconds = Math.floor(timeLeft - 60 * minutes);
+
   return (
-    <div>Timer</div>
-  )
-}
+    <div className="container">
+      <Block param="Minutes" number={minutes}/><h1>: </h1><Block param="Seconds" number={seconds}/>
+    </div>
+  );
+};
 
-export default Timer
+export default Timer;
