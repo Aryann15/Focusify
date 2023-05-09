e001fc718b48d4424ada680a579df6bb492ec6a8
Aryann15 <rajlysm15@gmail.com>
2023-05-09 12:25:51 +0530

    using the useForm hook

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,43 @@
-import React from 'react'
-import { useForm } from "react-hook-form"
-import "./Form.css"
+import React from "react";
+import { useForm } from "react-hook-form";
+import "./Form.css";
 
 const Form = () => {
-  return (
-    <div className ="center" >
-        <p>Minutes:</p>
-        <form>
-            <input type="number"/>
-            <input type="submit" />
-        </form>
-    </div>
-  )
-}
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-export default Form
+  
+  return (
+    <div className="center">
+      <p>Minutes:</p>
+      <form noValidate>
+        <input
+          type="number"
+          {...register("minutes", {
+            required: {
+              value: true,
+              message:
+                "Enter the number of Minutes that you would like to FOCUS for Champ!",
+            },
+            min: {
+              value: 0.001,
+              message:
+                "You really wanna concentrate less than a milliesecond, COME ON!",
+            },
+            max: {
+              value: 180,
+              message: "Whoa! slow down there zuck, try taking breaks!",
+            },
+          })}
+        />
+        <input type="submit" />
+      </form>
+      {errors.minutes && <p>{errors.minutes.message}</p>}
+    </div>
+  );
+};
+
+export default Form;
